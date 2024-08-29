1110f91a60c1489e03594df80a5423142821bfe9
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-08-29 17:59:07 +0530

    hashset

--- HashSets.java
+++ HashSets.java
@@ -1,3 +1,21 @@
+import java.util.HashSet;
+import java.util.Set;
 public class HashSets{
 // Prepare Hashset object here and add person class objects in them iterate it by using for each
+
+
+ public class person {
+    public static void main(String[] args) {
+        Set<Person> person1 = new HashSet<>();
+        person1.add(new Person("vijay", 20));
+        person1.add(new Person("tobi", 23));
+        person1.add(new Person("marsh", 26));
+   
+        for(person kl:person1){
+            System.out.println(kl);
+        }
+    }
+    }
 }
+
+
--- LinkedHashSets.java
+++ LinkedHashSets.java
@@ -1,4 +1,25 @@
+import java.util.Iterator;
+import java.util.LinkedHashSet;
+import java.util.Set;
 public class LinkedHashSets{
-public static void main(String[]args){
+
   // Preapre LinkedHashSets Object and Load Person Objects in it and iterate it using Iterator and While Loop
+
+public class main {
+    public static void main(String[] args) {
+        Set<Person> pe = new LinkedHashSet<>();
+
+        pe.add(new Person("mello", 20));
+        pe.add(new Person("Boby", 21));
+        pe.add(new Person("Charan", 23));
+
+        Iterator<Person> iterator = pe.iterator();
+        while (iterator.hasNext()) {
+            Person p = iterator.next();
+            System.out.println(p);
+        }
+    }
 }
+
+}
+
